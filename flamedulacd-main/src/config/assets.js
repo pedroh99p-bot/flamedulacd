4d55fa858b285e23af3fbb0e919f41aa7d6f5d30
luthguilherme <luthguilherme@gmail.com>
2026-08-02 20:18:21 -0300

    remove blocked Cloudinary backgrounds

--- flamedulacd-main/src/config/assets.js
+++ flamedulacd-main/src/config/assets.js
@@ -5,5 +5,4 @@ export const assets = {
   logoRedome: "https://redome.inca.gov.br/wp-content/themes/redome-tema/assets/svg/logo-redome-compacta.svg",
   introVideo: "https://res.cloudinary.com/wzzu3kvb/video/upload/f_auto,q_auto/WhatsApp_Video_2026-08-02_at_15.33.58_n6h5fw.mp4",
   infoVideo: "https://res.cloudinary.com/wzzu3kvb/video/upload/f_auto,q_auto/WhatsApp_Video_2026-08-02_at_15.33.58_n6h5fw.mp4",
-  quemSomosBackground: "https://res.cloudinary.com/dm9mnc97u/image/upload/v1781324232/bb202546-fd07-4d71-ba37-e16a618979da_d4bdjo.jpg"
 };
